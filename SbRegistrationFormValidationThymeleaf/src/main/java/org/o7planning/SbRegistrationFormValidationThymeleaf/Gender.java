bd3077b445f9d88332a9e32379446e681fdddc3f
sravancharlakola <sravanreddy.charl@gmail.com>
2018-07-29 22:50:27 -0400

    First Commit

---
+++ SbRegistrationFormValidationThymeleaf/src/main/java/org/o7planning/SbRegistrationFormValidationThymeleaf/Gender.java
@@ -0,0 +1,7 @@
+package org.o7planning.SbRegistrationFormValidationThymeleaf;
+
+public class Gender {
+	 
+    public static final String MALE = "M";
+    public static final String FEMALE = "F";
+}
